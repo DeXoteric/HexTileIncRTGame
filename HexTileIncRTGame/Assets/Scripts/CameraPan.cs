15be931c86661af4c093fbb092f65ddd93bce893
DeXoteric <marlev@poczta.pl>
2018-11-12 14:49:58 +0100

    camera pan

---
+++ HexTileIncRTGame/Assets/Scripts/CameraPan.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    [SerializeField] private float cameraMousePanSpeed;
+    [SerializeField] private float cameraMobilePanSpeed;
+
+    private float cameraPanSpeed;
+
+    private Vector2 mouseStart, mouseMove;
+
+    private Vector3 touchStart;
+
+    private void Start()
+    {
+        if (GameManager.instance.IsOnMobile)
+        {
+            cameraPanSpeed = cameraMobilePanSpeed;
+        } else
+        {
+            cameraPanSpeed = cameraMousePanSpeed;
+        }
+    }
+
+    private void Update()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = new Vector2(Input.mousePosition.x - touchStart.x, Input.mousePosition.y - touchStart.y);
+            touchStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.position = new Vector3(transform.position.x - direction.x * cameraPanSpeed * Time.deltaTime, transform.position.y, transform.position.z - direction.y * cameraPanSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ HexTileIncRTGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public bool IsOnMobile { get; set; }
+
+    private void Awake()
+    {
+        instance = this;
+        CheckIfOnMobile();
+    }
+
+    private void CheckIfOnMobile()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        IsOnMobile = true;
+#endif
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
+        IsOnMobile = false;
+#endif
+    }
+}
